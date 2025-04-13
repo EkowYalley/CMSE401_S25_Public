5a8d3fc8fa7a23966eaecd9d7ecb314d1507fb2b
Ekow Yalley <yalleyek@dev-amd20-v100.i>
2025-04-13 18:50:57 -0400

    HW4 part 3

--- HW4_STUDENT/reverseGOL-mpi.c
+++ HW4_STUDENT/reverseGOL-mpi.c
@@ -3,14 +3,15 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <mpi.h>
 
 #define max_nodes 264       /* Max number of nodes that we should test */
 #define str_length 50       /* Largest string that can be used for hostnames */
 
-int fitness(char * plate1, char * plate2, int n){
+int fitness(char * plate1, char * plate2, int n) {
     int errors = 0;
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n; j++){
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
             int index = i * (n + 2) + j;
             errors += !plate1[index]==plate2[index]; 
         }
@@ -18,7 +19,7 @@ int fitness(char * plate1, char * plate2, int n){
     return errors;
 }
 
-int live(int index, char * plate[2], int which, int n){
+int live(int index, char * plate[2], int which, int n) {
     return (plate[which][index - n - 3] 
         + plate[which][index - n - 2]
         + plate[which][index - n - 1]
@@ -29,15 +30,15 @@ int live(int index, char * plate[2], int which, int n){
         + plate[which][index + n + 3]);
 }
 
-int iteration(char * plate[2], int which, int n){
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n; j++){
+int iteration(char * plate[2], int which, int n) {
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
             int index = i * (n + 2) + j;
             int num = live(index, plate, which, n);
-            if(plate[which][index]){
+            if(plate[which][index]) {
                 plate[!which][index] =  (num == 2 || num == 3) ?
                     1 : 0;
-            }else{
+            } else {
                 plate[!which][index] = (num == 3);
             }
         }
@@ -46,57 +47,55 @@ int iteration(char * plate[2], int which, int n){
     return which;
 }
 
-void print_plate(char * plate, int n){
+void print_plate(char * plate, int n) {
     if (n < 60) {
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-		printf("%d", (int) plate[i * (n + 2) + j]);
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                printf("%d", (int) plate[i * (n + 2) + j]);
             }
             printf("\n");
         }
     } else {
-	printf("Plate too large to print to screen\n");
+        printf("Plate too large to print to screen\n");
     }
     printf("\0");
 }
 
 void makerandom(char * plate, int n) {
-  memset(plate, 0, sizeof(char));
-  for(int i = 1; i <= n; i++)
-     for(int j = 0; j < n; j++)
-         plate[i * (n+2) +j + 1] =  (rand() % 100) > 10;
-  return;
+    memset(plate, 0, sizeof(char));
+    for(int i = 1; i <= n; i++)
+        for(int j = 0; j < n; j++)
+            plate[i * (n+2) +j + 1] = (rand() % 100) > 10;
+    return;
 }
 
 void mutate(char * plate, char * best_plate, int n, int rate) {
-  for(int i = 1; i <= n; i++)
-     for(int j = 0; j < n; j++) {
-         int index = i * (n+2) +j + 1;    
-         if ((rand() %  100) < rate) {
-             plate[index] =  (rand() % 2) > 0;
-	  } else {
-             plate[index] =  best_plate[index];
-          }
-      }
-  return; 
+    for(int i = 1; i <= n; i++)
+        for(int j = 0; j < n; j++) {
+            int index = i * (n+2) +j + 1;    
+            if ((rand() % 100) < rate) {
+                plate[index] = (rand() % 2) > 0;
+            } else {
+                plate[index] = best_plate[index];
+            }
+        }
+    return; 
 }
 
 void cross(char * plate1, char * plate2, int n) {
-  int start = 0;
-  int end = (n+2)*(n+2);
-  int crosspoint = rand() % end;
-  if ( crosspoint < end/2) {
-	start = 0;
+    int start = 0;
+    int end = (n+2)*(n+2);
+    int crosspoint = rand() % end;
+    if (crosspoint < end/2) {
+        start = 0;
         end = crosspoint;
-  } else {
-	start = crosspoint;
-  }
-  for(int i = start; i <= end; i++)	
-      plate1[i] =  plate2[i];
-
-  return;  
+    } else {
+        start = crosspoint;
+    }
+    for(int i = start; i <= end; i++)     
+        plate1[i] = plate2[i];
+    return;  
 } 
-
 
 char * readplate(char * filename, int *n) {
     char * plate;
@@ -105,23 +104,23 @@ char * readplate(char * filename, int *n) {
     FILE *fp; 
     printf("Reading %s\n",filename);
     if ((fp = fopen(filename, "r")) == NULL) {
-      perror("Error opening file");
-      exit(EXIT_FAILURE);
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
     }
-    if(fscanf(fp, "%d %d", &N, &M) == 2){
+    if(fscanf(fp, "%d %d", &N, &M) == 2) {
         printf("Reading in %dx%d array\n",N,N);
         plate = (char *) calloc((N+2)*(N*2),sizeof(char)); 
         char line[N];
-        for(int i = 1; i <= N; i++){
-                fscanf(fp, "%s", &line);
-                for(int j = 0; j < N; j++){
-                    int index = i * (N + 2) + j + 1;
-                    plate[index] = (line[j] == '1');
-                }
+        for(int i = 1; i <= N; i++) {
+            fscanf(fp, "%s", &line);
+            for(int j = 0; j < N; j++) {
+                int index = i * (N + 2) + j + 1;
+                plate[index] = (line[j] == '1');
+            }
         }
         *n = N;
     } else {
-	printf("File not correct format\n");
+        printf("File not correct format\n");
         exit(EXIT_FAILURE);
     }
     fclose(fp);
@@ -129,9 +128,14 @@ char * readplate(char * filename, int *n) {
 }
 
 int main(int argc, char *argv[]) {
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
     int which = 0;
     int n;
-    int npop = 10000;
+    int npop = 10000 / size;  // Divide population among workers
     int ngen = 1000;
     int M=0;
     int rand_seed;
@@ -140,87 +144,110 @@ int main(int argc, char *argv[]) {
     if (argc > 2)
         rand_seed = (atoi(argv[2])+1)*7;
     else
-	rand_seed = (unsigned int) time(&t);
-    printf("Random Seed = %d\n", rand_seed);
+        rand_seed = (unsigned int) time(&t) + rank;  // Different seed for each worker
+
+    printf("Worker %d: Random Seed = %d\n", rank, rand_seed);
     srand(rand_seed);
 
-    printf("%d %d %d %d\n", rand() % 100, rand() % 100, rand() % 100, rand() % 100);
     char * test_plate;
     char * buffer_plate; 
     char * target_plate;  
  
-    target_plate = readplate(argv[1], &n);    
-    buffer_plate = (char *) calloc((n+2)*(n+2),sizeof(char)); 
-
+    if (rank == 0) {
+        target_plate = readplate(argv[1], &n);
+    }
+    
+    // Broadcast the plate size to all workers
+    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    
+    if (rank != 0) {
+        target_plate = (char *) calloc((n+2)*(n+2), sizeof(char));
+    }
+    
+    // Broadcast the target plate to all workers
+    MPI_Bcast(target_plate, (n+2)*(n+2), MPI_CHAR, 0, MPI_COMM_WORLD);
+    
+    buffer_plate = (char *) calloc((n+2)*(n+2), sizeof(char)); 
     char * population[npop];
     int pop_fitness[npop];
-    int best =0;
+    int best = 0;
     int sbest = 1;
-
+    
     for(int i=0; i < npop; i++) {
         pop_fitness[i] = n*n;
-	population[i] = (char *) calloc((n+2)*(n+2),sizeof(char)); 
+        population[i] = (char *) calloc((n+2)*(n+2), sizeof(char)); 
         if (i < npop/2)
-	    mutate(population[i], target_plate,  n, 10); 
-	else
+            mutate(population[i], target_plate, n, 10); 
+        else
             makerandom(population[i], n);
     }
 
     for(int g=0; g < ngen; g++) {
-	for(int i=0; i<npop; i++) {
-	    char *plate[2];
-    	    plate[0] = population[i];
-    	    plate[1] = buffer_plate;
-    	    iteration(plate, 0, n);
-
+        for(int i=0; i<npop; i++) {
+            char *plate[2];
+            plate[0] = population[i];
+            plate[1] = buffer_plate;
+            iteration(plate, 0, n);
             pop_fitness[i] = fitness(buffer_plate, target_plate, n);
- 
-	    if (pop_fitness[i] < pop_fitness[best]) { 
-               sbest = best;
-	       best = i;    
-               if (pop_fitness[best] == 0) {
-                   printf("Perfect previous plate found\n");
-		   char * temp = target_plate;
-	           target_plate = population[best];
- 		   population[best] = temp;
-		   printf("%d %d\n", n, M);
-                   print_plate(target_plate, n);
-                   pop_fitness[best] = n*n;
-                   M++;
-               }                 
+
+            if (pop_fitness[i] < pop_fitness[best]) { 
+                sbest = best;
+                best = i;    
+                if (pop_fitness[best] == 0) {
+                    printf("Worker %d: Perfect previous plate found\n", rank);
+                    char * temp = target_plate;
+                    target_plate = population[best];
+                    population[best] = temp;
+                    printf("%d %d\n", n, M);
+                    print_plate(target_plate, n);
+                    pop_fitness[best] = n*n;
+                    M++;
+                }                 
             } else {
-		if (sbest == best) 
-		    sbest = i;
-	    }
-	}
-
-        printf("Done with Generation %d with best=%d fitness=%d\n", g,best, pop_fitness[best]);
-	
+                if (sbest == best) 
+                    sbest = i;
+            }
+        }
+        printf("Worker %d: Generation %d with best=%d fitness=%d\n", rank, g, best, pop_fitness[best]);
+        
         int rate = (int) ((double) pop_fitness[best]/(n*n) * 100);
-
         for(int i=0; i <npop; i++) {
             if (i == sbest) {
-		cross(population[i], population[best],  n); 
-		sbest = 1;
-	    } else if (i != best) {
+                cross(population[i], population[best], n); 
+                sbest = 1;
+            } else if (i != best) {
                 if (i < npop/3) // mutate top 1/3 based on best
-		   mutate(population[i], population[best],  n, rate); 
-		else if (i < (npop*2)/3)  // cross with next 1/3 
-		   cross(population[i], population[best],n);
-		else // Last 1/3 is new random numbers. 
-		   makerandom(population[i], n);
-	    }
-	}
+                    mutate(population[i], population[best], n, rate); 
+                else if (i < (npop*2)/3)  // cross with next 1/3 
+                    cross(population[i], population[best], n);
+                else // Last 1/3 is new random numbers. 
+                    makerandom(population[i], n);
+            }
+        }
     }
 
-    printf("%d %d\n",n,  M+1);
-    print_plate(population[best], n);
-    printf("\nResult Fitness=%d over %d iterations:\n",pop_fitness[best], ngen);
+    // Gather results from all workers
+    int global_best_fitness;
+    int local_best_fitness = pop_fitness[best];
+    MPI_Reduce(&local_best_fitness, &global_best_fitness, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
 
+    if (rank == 0) {
+        printf("\nOverall best fitness across all workers: %d over %d iterations:\n", global_best_fitness, ngen);
+    }
+
+    // If we want to get the actual best plate from any worker, we'd need more complex communication
+    // This would involve:
+    // 1. Each worker sending its best fitness to rank 0
+    // 2. Rank 0 determining which worker has the best fitness
+    // 3. That worker sending its best plate to rank 0
+    // 4. Rank 0 printing that plate
+
+    // Free memory
     free(target_plate);
     free(buffer_plate);
     for(int i=0; i < npop; i++)
-	free(population[i]);
+        free(population[i]);
 
+    MPI_Finalize();
     return 0;
 }
